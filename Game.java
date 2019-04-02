2e6519e23147df42fa4e5465ee7363edcfa074a4
Mircea Ion <49190686+imicodes@users.noreply.github.com>
2019-04-02 12:27:53 +0100

    Create Game.java

---
+++ Game.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Mircea
+ */
+public class Game {
+
+    /**
+     * @param args the command line arguments
+     */
+    static JFrame jf1 = new NewJFrame();
+    public static void main(String[] args) {
+        // TODO code application logic here
+        jf1.setVisible(true);
+    }
+    
+}
